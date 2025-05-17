2f8d98b5d6e9f3be4b03edae163b3dea0b1e28eb
night-pro13 <facknet1999@gmail.com>
2025-05-17 21:17:05 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,17 +1,17 @@
 import os
 
 class Config(object):
-  API_ID = int(os.environ.get("API_ID", ""))
-  API_HASH = os.environ.get("API_HASH", "")
+  API_ID = int(os.environ.get("API_ID", "25230605"))
+  API_HASH = os.environ.get("API_HASH", "b7d6c13e37d52cbbea25742f1c8b40cd")
   BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-  BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
-  DB_CHANNEL = int(os.environ.get("DB_CHANNEL", ""))
-  SHORTLINK_URL = os.environ.get('SHORTLINK_URL', "MoneyKamalo.com")
-  SHORTLINK_API = os.environ.get('SHORTLINK_API', "0eefb93e1e3ce9470a7033115ceb1bad13a9d674")
-  BOT_OWNER = int(os.environ.get("BOT_OWNER", ""))
-  DATABASE_URL = os.environ.get("DATABASE_URL", "")
+  BOT_USERNAME = os.environ.get("BOT_USERNAME", "@Mms_hub18_bot")
+  DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "1002564863779"))
+  SHORTLINK_URL = os.environ.get('SHORTLINK_URL', "bharatlinks.com")
+  SHORTLINK_API = os.environ.get('SHORTLINK_API', "229853ecbbbbd01d73da405efce80c3acb8654ca")
+  BOT_OWNER = int(os.environ.get("BOT_OWNER", "6392951002"))
+  DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://facknet1999:GjMN6ZY5R3AbPx56@cluster0.6a3fnf0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
   UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
-  LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+  LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "1002663679629"))
   BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
   FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
   BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
@@ -23,7 +23,7 @@ Send Me any Media or File. I can Work In Channel too. Add Me to Channel with Edi
 
 ╭────[ 🔅FɪʟᴇSᴛᴏʀᴇBᴏᴛ🔅]────⍟
 │
-├🔸 My Name: [FileStore Bot](https://t.me/{BOT_USERNAME})
+├🔸 My Name: [FileStore Bot](https://t.me/{Mms_hub18_bot})
 │
 ├🔸 Language: [Python 3](https://www.python.org)
 │
